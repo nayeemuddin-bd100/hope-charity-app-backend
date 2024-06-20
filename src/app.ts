20fe4b73807491d61e517c2d6617755f0302c973
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-20 19:39:34 +0600

    setup ts,express,mongoose

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+// import globalErrorHandler from "./app/middleware/globalErrorHandler";
+// import routers from "./app/routes";
+
+const app: Application = express();
+app.use(cors());
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//app route
+// app.use("/api/v1/users/", userRoute);
+// app.use("/api/v1", routers);
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+//global Error handler
+// app.use(globalErrorHandler);
+
+//handle Not Found Route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    statusCode: StatusCodes.NOT_FOUND,
+    success: false,
+    message: "API Not Found",
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
