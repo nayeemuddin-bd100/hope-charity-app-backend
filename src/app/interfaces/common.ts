4355007cf471d3cfb118753322d09706dbbe3403
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-24 02:59:13 +0600

    feat: add common utils

--- src/app/interfaces/common.ts
+++ src/app/interfaces/common.ts
@@ -1,5 +1,14 @@
 import { IGenericErrorMsg } from './error'
 
+export type IGenericResponse<T> = {
+  meta: {
+    page: number
+    limit: number
+    total: number
+  }
+  data: T | null
+}
+
 export type IGenericMsgResponse = {
   statusCode: number
   message: string
---
+++ src/app/modules/user/user.constant.ts
@@ -0,0 +1,7 @@
+export const userFilterableFields = ['searchTerm', 'email', 'role']
+export const userSearchableFields = [
+  'name.firstName',
+  'name.lastName',
+  'email',
+  'role',
+]
