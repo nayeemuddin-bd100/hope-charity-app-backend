65e4f4acde2b1ac2ab5356b991efac30c1722fb6
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-23 22:42:24 +0600

    feat: add zod error in global error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,6 +1,8 @@
 import { ErrorRequestHandler } from 'express'
+import { ZodError } from 'zod'
 import config from '../../config'
 import ApiError from '../../errors/ApiError'
+import handleZodError from '../../errors/handleZodError'
 import { IGenericErrorMsg } from '../interfaces/error'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -11,7 +13,13 @@ const globalErrorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
   let message = 'Something went wrong!'
   let errorMessages: IGenericErrorMsg[] = []
 
-  if (error instanceof ApiError) {
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error)
+
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorMessages = simplifiedError?.errorMessages
+  } else if (error instanceof ApiError) {
     statusCode = error?.statusCode
     message = error?.message
     errorMessages = error?.message
