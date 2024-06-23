e856fa683e7ce32f98220722cbc7ab3ff9a1ebf8
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-24 03:00:37 +0600

    fix: nullable value error fix

--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -17,8 +17,8 @@ const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
     statusCode: data.statusCode,
     success: data.success,
     message: data.message || null,
-    meta: data.meta || undefined,
-    data: data.data || null,
+    meta: data?.meta || undefined,
+    data: data?.data || null,
   }
 
   res.status(data.statusCode).json(responseData)
