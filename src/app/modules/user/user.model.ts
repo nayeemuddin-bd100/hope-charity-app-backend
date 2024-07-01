7fcec08f8909362c2469c35fa9c4eb3627545816
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-02 00:47:24 +0600

    feat: add bcrypt for password hashing

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,4 +1,6 @@
+import bcrypt from 'bcrypt'
 import { Schema, model } from 'mongoose'
+import config from '../../../config'
 import { IUser, UserModel } from './user.interface'
 
 const userSchema = new Schema<IUser, UserModel>(
@@ -17,15 +19,15 @@ const userSchema = new Schema<IUser, UserModel>(
       required: true,
     },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     role: {
       type: String,
       enum: ['admin', 'super-admin', 'volunteer', 'donor'],
       default: 'donor',
     },
     admin: { type: Schema.Types.ObjectId, ref: 'Admin' },
-    // donor: { type: Schema.Types.ObjectId, ref: 'Donor' },
-    // volunteer: { type: Schema.Types.ObjectId, ref: 'Volunteer' },
+    donor: { type: Schema.Types.ObjectId, ref: 'Donor' },
+    volunteer: { type: Schema.Types.ObjectId, ref: 'Volunteer' },
   },
   {
     timestamps: true,
@@ -34,5 +36,14 @@ const userSchema = new Schema<IUser, UserModel>(
     },
   },
 )
+
+// hash password before saving
+userSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcrypt_salt_round),
+  )
+  next()
+})
 
 export const User = model<IUser, UserModel>('User', userSchema)
