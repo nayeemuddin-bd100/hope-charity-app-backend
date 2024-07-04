11b8f96c0d3e0152a9c3aa0281448e66e4d9ce97
nayeem <nayeemuddin.bd100@gmail.com>
2024-07-04 13:24:10 +0600

    feat: add cause route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { adminRoute } from '../modules/admin/admin.route'
 import { authRoute } from '../modules/auth/auth.route'
+import { causeRoute } from '../modules/cause/cause.route'
 import { donorRoute } from '../modules/donor/donor.route'
 import { userRoute } from '../modules/user/user.route'
 import { volunteerRoute } from '../modules/volunteer/volunteer.route'
@@ -27,6 +28,11 @@ const moduleRoute = [
   {
     path: '/volunteer',
     route: volunteerRoute,
+  },
+
+  {
+    path: '/cause',
+    route: causeRoute,
   },
 ]
 
